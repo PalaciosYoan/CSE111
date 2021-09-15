f69046178c6cc98ce483893708bf586b2dcb8b2a
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-09-14 22:00:51 -0700

    Finish queries 1-3

--- lab-3/queries.sql
+++ lab-3/queries.sql
@@ -1,3 +1,14 @@
 SELECT c_name, c_phone, c_address, c_acctbal 
 FROM customer 
 WHERE c_name='Customer#000000010';
+
+
+SELECT min(c_acctbal) FROM customer;
+
+SELECT l_receiptdate, l_returnflag, l_extendedprice, l_tax 
+FROM lineitem 
+WHERE l_returnflag='R' 
+intersect 
+SELECT l_receiptdate, l_returnflag, l_extendedprice, l_tax 
+FROM lineitem 
+WHERE l_receiptdate='1993-08-22';
