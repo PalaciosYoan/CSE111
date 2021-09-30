79f9daa835d8ebd0ef3e2d0bbda5b7f1cff04c57
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-09-30 13:33:20 -0700

    lab4: doing 13

--- lab-4/test/13.sql
+++ lab-4/test/13.sql
@@ -2,3 +2,10 @@
 -- by  suppliers  in AFRICA for  orders  made  
 -- by  customers  in UNITEDSTATES?
 
+SELECT COUNT(l_orderkey)
+FROM lineitem, supplier, nation, region, orders
+WHERE 
+    s_suppkey = l_suppkey AND
+    s_nationkey = n_nationkey AND
+    n_regionkey = r_regionkey AND
+    o
