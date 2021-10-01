71b4476741e87a8358989c3186b042b8b09c57b2
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-09-30 20:04:27 -0700

    ghdfh

--- lab-4/score.res
+++ lab-4/score.res
@@ -11,6 +11,6 @@ Query 10: PASS
 Query 11: PASS
 Query 12: PASS
 Query 13: PASS
-Query 14: FAIL
+Query 14: PASS
 Query 15: PASS
-Total score: 14
+Total score: 15
--- lab-4/test/14.sql
+++ lab-4/test/14.sql
@@ -3,3 +3,26 @@
 -- are from one region and the customers are 
 -- from the other region.
 
+SELECT  t1.r_name, t2.r_name, MAX(o_totalprice)
+FROM 
+(
+    SELECT *
+    FROM supplier, region, nation, lineitem
+    WHERE 
+        s_suppkey = l_suppkey AND
+        s_nationkey = n_nationkey AND
+        n_regionkey = r_regionkey
+) t1,
+(
+    SELECT *
+    FROM orders, customer, region, nation
+    WHERE 
+        n_regionkey = r_regionkey AND
+        c_nationkey = n_nationkey AND
+        o_custkey = c_custkey
+) t2
+WHERE
+    o_orderkey = l_orderkey
+GROUP BY 
+    t1.r_name, t2.r_name
+
