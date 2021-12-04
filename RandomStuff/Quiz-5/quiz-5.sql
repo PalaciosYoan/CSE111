8878490c8382403ebf85d636096a42610f45a48d
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-12-03 17:26:14 -0800

    two potatoes

--- RandomStuff/Quiz-5/quiz-5.sql
+++ RandomStuff/Quiz-5/quiz-5.sql
@@ -1,4 +1,8 @@
 PRAGMA foreign_keys = on;
+DROP TABLE IF EXISTS Classes;
+DROP TABLE IF EXISTS Ships;
+DROP TABLE IF EXISTS Battles;
+DROP TABLE IF EXISTS Outcomes;
 
 SELECT "1----------";
 .headers on
@@ -26,8 +30,8 @@ create table Battles(
 );
 
 create table Outcomes(
-    ship CHAR(32) REFERENCES Ships(name) ON DELETE CASCADE,
-    battle CHAR(32) REFERENCES Battles(name) ON DELETE CASCADE,
+    ship CHAR(32) REFERENCES Ships(name) ON DELETE CASCADE ON UPDATE CASCADE,
+    battle CHAR(32) REFERENCES Battles(name) ON DELETE CASCADE ON UPDATE CASCADE,
     result CHAR(32),
     CHECK (result IN ('ok', 'sunk', 'damaged'))
 )
@@ -37,6 +41,52 @@ create table Outcomes(
 SELECT "2----------";
 .headers on
 --put your code here
+INSERT INTO Classes VALUES ('Bismarck', 'bb', 'Germany', 8, 15, 42000);
+INSERT INTO Classes VALUES ('Iowa', 'bb', 'USA', 9, 16, 46000);
+INSERT INTO Classes VALUES ('Kongo', 'bc', 'Japan', 8, 14, 32000);
+INSERT INTO Classes VALUES ('North Carolina', 'bb', 'USA', 9, 16, 37000);
+INSERT INTO Classes VALUES ('Renown', 'bc', 'Britain', 6, 15, 32000);
+INSERT INTO Classes VALUES ('Revenge', 'bb', 'Britain', 8, 15, 29000);
+INSERT INTO Classes VALUES ('Tennessee', 'bb', 'USA', 12, 14, 32000);
+INSERT INTO Classes VALUES ('Yamato', 'bb', 'Japan', 9, 18, 65000);
+
+
+INSERT INTO Ships VALUES ('California', 'Tennessee',  1915);
+INSERT INTO Ships VALUES ('Haruna', 'Kongo',  1915);
+INSERT INTO Ships VALUES ('Hiei', 'Kongo',  1915);
+INSERT INTO Ships VALUES ('Iowa', 'Iowa',  1933);
+INSERT INTO Ships VALUES ('Kirishima', 'Kongo',  1915);
+INSERT INTO Ships VALUES ('Kongo', 'Kongo',  1913);
+INSERT INTO Ships VALUES ('Missouri', 'Iowa',  1935);
+INSERT INTO Ships VALUES ('Musashi', 'Yamato',  1942);
+INSERT INTO Ships VALUES ('New Jersey', 'Iowa',  1936);
+INSERT INTO Ships VALUES ('North Carolina', 'North Carolina',  1941);
+INSERT INTO Ships VALUES ('Ramillies', 'Revenge',  1917);
+INSERT INTO Ships VALUES ('Renown', 'Renown',  1916);
+INSERT INTO Ships VALUES ('Repulse', 'Renown',  1916);
+INSERT INTO Ships VALUES ('Resolution', 'Revenge',  1916);
+INSERT INTO Ships VALUES ('Revenge', 'Revenge',  1916);
+INSERT INTO Ships VALUES ('Royal Oak', 'Revenge',  1916);
+INSERT INTO Ships VALUES ('Royal Sovereign', 'Revenge',  1916);
+INSERT INTO Ships VALUES ('Tennessee', 'Tennessee',  1915);
+INSERT INTO Ships VALUES ('Washington', 'North Carolina',  1941);
+INSERT INTO Ships VALUES ('Wisconsin', 'Iowa',  1940);
+INSERT INTO Ships VALUES ('Yamato', 'Yamato',  1941);
+
+INSERT INTO Battles VALUES ('Denmark Strait', '1941-05-24');
+INSERT INTO Battles VALUES ('Guadalcanal', '1942-11-15');
+INSERT INTO Battles VALUES ('North Cape', '1943-12-26');
+INSERT INTO Battles VALUES ('Surigao Strait', '1944-10-25');
+
+INSERT INTO Outcomes VALUES ('California', 'Surigao Strait',  'ok');
+INSERT INTO Outcomes VALUES ('Kirishima', 'Guadalcanal',  'sunk');
+INSERT INTO Outcomes VALUES ('Resolution', 'Denmark Strait',  'ok');
+INSERT INTO Outcomes VALUES ('Wisconsin', 'Guadalcanal',  'damaged');
+INSERT INTO Outcomes VALUES ('Tennessee', 'Surigao Strait',  'ok');
+INSERT INTO Outcomes VALUES ('Washington', 'Guadalcanal',  'ok');
+INSERT INTO Outcomes VALUES ('New Jersey', 'Surigao Strait',  'ok');
+INSERT INTO Outcomes VALUES ('Yamato', 'Surigao Strait',  'sunk');
+INSERT INTO Outcomes VALUES ('Wisconsin', 'Surigao Strait',  'damaged');
 ;
 
 select * from Classes;
@@ -48,6 +98,11 @@ select * from Outcomes;
 SELECT "3----------";
 .headers on
 --put your code here
+DELETE 
+FROM Classes
+WHERE
+    displacement < 30000 OR
+    numGuns < 8
 ;
 
 select * from Classes;
@@ -57,6 +112,10 @@ select * from Ships;
 SELECT "4----------";
 .headers on
 --put your code here
+DELETE 
+FROM Battles
+WHERE
+    name = "Guadalcanal"
 ;
 
 select * from Battles;
@@ -66,6 +125,11 @@ select * from Outcomes;
 SELECT "5----------";
 .headers on
 --put your code here
+UPDATE Battles
+SET 
+    name = 'Strait of Surigao'
+WHERE 
+    name = 'Surigao Strait';
 ;
 
 select * from Battles;
@@ -75,6 +139,9 @@ select * from Outcomes;
 SELECT "6----------";
 .headers on
 --put your code here
+DELETE
+FROM Ships
+WHERE class IS NULL
 ;
 
 select * from Ships;
